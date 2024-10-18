3c50e0aa86671b7c5a0ca816913220463f2f146c
José Bauer <jose.h.bauer03@gmail.com>
2024-10-18 11:12:23 -0300

    Cria tabelas referente ao armazenamento de receitas

--- src/database/initializeDatabase.ts
+++ src/database/initializeDatabase.ts
@@ -6,5 +6,63 @@ export async function initializeDatabase(database: SQLiteDatabase) {
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL
     );
-  `);
+  `)
+
+  await database.execAsync(`
+    CREATE TABLE IF NOT EXISTS recipes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT,
+      instructions TEXT,
+      category_id INTEGER,
+      FOREIGN KEY (category_id) REFERENCES categories(id)
+    );  
+  `)
+
+  await database.execAsync(`
+    CREATE TABLE IF NOT EXISTS ingredients (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL
+    );
+  `)
+
+  await database.execAsync(`
+    CREATE TABLE IF NOT EXISTS measures (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      unit TEXT NOT NULL
+    );
+  `)
+
+  const defaultMeasures = [
+    "xícaras",
+    "ml",
+    "litros",
+    "kg",
+    "gramas",
+    "unidade",
+    "colheres de sopa",
+    "colheres de chá",
+    "pacote",
+    "pedaço",
+    "caixinha"
+  ]
+
+  for (const unit of defaultMeasures) {
+    await database.execAsync(`
+      INSERT OR IGNORE INTO measures (unit) VALUES ('${unit}');
+    `)
+  }
+
+  await database.execAsync(`
+    CREATE TABLE IF NOT EXISTS recipe_ingredients (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      recipe_id INTEGER NOT NULL,
+      ingredient_id INTEGER NOT NULL,
+      quantity REAL NOT NULL,
+      measure_id INTEGER NOT NULL,
+      FOREIGN KEY (recipe_id) REFERENCES recipes(id),
+      FOREIGN KEY (ingredient_id) REFERENCES ingredients(id),
+      FOREIGN KEY (measure_id) REFERENCES measures(id)
+    );
+  `)
 }
